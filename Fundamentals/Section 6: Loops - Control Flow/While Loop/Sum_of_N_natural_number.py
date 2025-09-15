3d6bc88537d56861d23cf1a84251ca630c3c33d5
Krishi <krishiminergate@gmail.com>
2025-09-15 15:47:56 +0530

    Revert "While Loop Fundamental"
    This reverts commit f9fbc6f10ac1e38fda6464811ba9408fe97acbaa.

--- Fundamentals/Section 6: Loops - Control Flow/While Loop/Sum_of_N_natural_number.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Total number of natural number to be added: "))
-sum = 0
-i = 0
-while num > i:
-    sum += int(input("Enter number: "))
-    i += 1
-
-print(f"Sum for {num} natural number is: {sum}")
